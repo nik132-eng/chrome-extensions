90d43e287402078ea981eac922246034f5358ef8
Nikunj Rohit <nikunjrohit10@gmail.com>
2023-11-07 09:16:31 +0530

    change some button and add css

--- scripts/content.js
+++ scripts/content.js
@@ -1,1 +1,7 @@
-console.log("Hello world")
+var button = document.createElement("button");
+button.id = "BtnBQ";
+document.body.appendChild(button);
+
+// button.addEventListener("click", function() {
+//   alert("Button clicked!");
+// });
