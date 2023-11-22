9247e2828892035f58d07c223f3e9a8b76b240c3
Nikunj Rohit <nikunjrohit10@gmail.com>
2023-11-22 09:24:23 +0530

    Fun- add listerners and animation on cllick

--- scripts/content.js
+++ scripts/content.js
@@ -1,7 +1,17 @@
-var button = document.createElement("button");
-button.id = "BtnBQ";
-document.body.appendChild(button);
+var body = document.querySelector("body")
 
-// button.addEventListener("click", function() {
-//   alert("Button clicked!");
-// });
+var btnBQ = document.createElement("button");
+btnBQ.setAttribute("id","BtnBQ");
+btnBQ.addEventListener("click", doSomething);
+
+body.appendChild(btnBQ);
+
+function doSomething(){
+    if(btnBQ.hasAttribute("listening") === false){
+        btnBQ.setAttribute("listening",true);
+        console.log("I'm listening");
+    }else{
+        btnBQ.removeAttribute("listening");
+        console.log("i'm not listening");
+    }
+}
