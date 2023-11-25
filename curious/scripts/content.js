d92bbb054fd1ca85fe5a4d8c6cbcf64c844dc17a
Nikunj Rohit <nikunjrohit10@gmail.com>
2023-11-25 17:07:30 +0530

    add speech recognized api

--- curious/scripts/content.js
+++ curious/scripts/content.js
@@ -1,17 +1,32 @@
 var body = document.querySelector("body")
 
 var btnBQ = document.createElement("button");
-btnBQ.setAttribute("id","BtnBQ");
+btnBQ.setAttribute("id", "BtnBQ");
 btnBQ.addEventListener("click", doSomething);
 
 body.appendChild(btnBQ);
 
-function doSomething(){
-    if(btnBQ.hasAttribute("listening") === false){
-        btnBQ.setAttribute("listening",true);
-        console.log("I'm listening");
-    }else{
+let recognition = new webkitSpeechRecognition();
+recognition.continuous = true;
+recognition.interimResults = true;
+recognition.lang = "en-us";
+
+let transcript = "";
+recognition.onresult = function (event) {
+    transcript = "";
+    for (var i = 0; i < event.results.length; ++i) {
+        transcript += event.results[i][0].transcript;
+    }
+}
+
+function doSomething() {
+    if (btnBQ.hasAttribute("listening") === false) {
+        btnBQ.setAttribute("listening", true);
+        recognition.start();
+    } else {
         btnBQ.removeAttribute("listening");
-        console.log("i'm not listening");
+        recognition.stop();
+        console.log("this is what you said -", transcript);
+        transcript = "";
     }
 }
